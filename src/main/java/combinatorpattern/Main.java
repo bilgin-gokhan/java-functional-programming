84ec53f2e957e19e86c29a2a4e0a36edc564992e
Gokhan <gokhanbilgin5@gmail.com>
2021-08-08 21:00:19 +0300

    Implementing Combinator Pattern

--- src/main/java/combinatorpattern/Main.java
+++ src/main/java/combinatorpattern/Main.java
@@ -1,17 +1,35 @@
 package combinatorpattern;
 
+import combinatorpattern.CustomerRegistrationValidator.ValidationResult;
+
 import java.time.LocalDate;
+
+import static combinatorpattern.CustomerRegistrationValidator.isAnAdult;
+import static combinatorpattern.CustomerRegistrationValidator.isPhoneNumberValid;
 
 public class Main {
 
     public static void main(String[] args) {
         Customer customer = new Customer(
                 "Alice",
-                "alice@xyz.com",
+                "alicexyz.com",
                 "+082947529845",
                 LocalDate.of(2000, 1, 1)
         );
 
         System.out.println(new CustomerValidatorService().isValid(customer));
+
+        // Using combinator pattern
+        ValidationResult result = CustomerRegistrationValidator
+                .isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAnAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 }
