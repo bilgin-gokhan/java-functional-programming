d0386c183f1a19b39f42b57420088a9096f0b353
Gokhan <gokhanbilgin5@gmail.com>
2021-08-08 12:26:31 +0300

    BiConsumer

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -9,15 +10,31 @@ public class _Consumer {
 
         // Normal Java function
         greetCustomer(maria);
+        greetCustomerV2(maria, false);
 
         // Consumer Functional interface
         greetCustomerConsumer.accept(maria);
+
+        // BiConsumer
+        greetCustomerConsumerV2.accept(maria, false);
     }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.customerName
+                    + ", thanks for registering phone number "
+                    + (showPhoneNumber ? customer.customerPhoneNumber : "*********"));
 
     static Consumer<Customer> greetCustomerConsumer = customer ->
             System.out.println("Hello " + customer.customerName
                     + ", thanks for registering phone number "
                     + customer.customerPhoneNumber);
+
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+        System.out.println("Hello " + customer.customerName
+                + ", thanks for registering phone number "
+                + (showPhoneNumber ? customer.customerPhoneNumber : "*********"));
+
+    }
 
     static void greetCustomer(Customer customer) {
         System.out.println("Hello " + customer.customerName
